771f2b0d9a5125645a7f7ffacf820920c6fbc324
SirCristian <crislprojects@gmail.com>
2023-10-21 23:08:54 -0300

    Squashed commit of the following:
    commit ed6c01eab262178fc21fb1106c1cf4b245327559
    Author: SirCristian <crislprojects@gmail.com>
    Date:   Sat Oct 21 23:07:20 2023 -0300
    
        Agregando func para llevar a prod.

--- script-3.js
+++ script-3.js
@@ -8,4 +8,6 @@ function Nuevadata(params) {
     // Estoy agregando una funcionalidad que debe estar en prod, pero a su vez prod tiene cosas que dev no tiene y no debe tener.
 }
 
-
+function otraFunción(params) {
+    // Estoy agregando otra función que necesito que se actualice en main, sin que yo traiga los cambios de prod.
+}
