f3dd78756731262cd6e75eaa63c06cbcfe0a85e3
SirCristian <crislprojects@gmail.com>
2023-10-21 22:46:18 -0300

    Versión de prod

---
+++ script-1.js
@@ -0,0 +1,7 @@
+function onlyProd() {
+    console.log("Soy una función que solo puedo estar en prod, nunca en Dev.");
+}
+
+function onlyProd2() {
+    console.log("Soy una función que solo puedo estar en prod, nunca en Dev.");
+}
