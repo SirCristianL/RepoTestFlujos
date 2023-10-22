8387d4f8f8d3a88f565614a57e621199655f1511
SirCristian <crislprojects@gmail.com>
2023-10-21 23:11:46 -0300

    Eliminada wea que solo debe estar en prod

--- script-3.js
+++ script-3.js
@@ -1,9 +1,3 @@
-alert("only prod");
-alert("only prod");
-alert("only prod");
-alert("only prod");
-alert("only prod");
-
 function Nuevadata(params) {
     // Estoy agregando una funcionalidad que debe estar en prod, pero a su vez prod tiene cosas que dev no tiene y no debe tener.
 }
