ff4ce39f1e3a92c5355b00b3f8424b380c09710a
SirCristian <crislprojects@gmail.com>
2023-10-21 23:06:26 -0300

    Data que solo está en prod

--- script-3.js
+++ script-3.js
@@ -1,3 +1,11 @@
+alert("only prod");
+alert("only prod");
+alert("only prod");
+alert("only prod");
+alert("only prod");
+
 function Nuevadata(params) {
     // Estoy agregando una funcionalidad que debe estar en prod, pero a su vez prod tiene cosas que dev no tiene y no debe tener.
 }
+
+
